248bc591dae482ce2a945bbc75eeeb71c64ac7b0
Pradeep Prajapati <pradeep.prajapati@i2econsulting.com>
2024-12-01 15:48:15 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,4 @@
+
+const parent = React.createElement( "div", {} , React.createElement( "div2", {} ,"Hare Krishna"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
